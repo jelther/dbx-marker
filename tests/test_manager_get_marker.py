bc49367b1a7227510bd10602274774e3e713dda3
Jelther Gonçalves <4515493+jelther@users.noreply.github.com>
2025-01-22 10:01:30 -0600

    Refactored get_marker method.

--- tests/test_manager_get_marker.py
+++ tests/test_manager_get_marker.py
@@ -24,9 +24,17 @@ def test_get_marker_that_exists(mock_table_exists, mock_spark):
 
 
 @patch("dbx_marker.manager.delta_table_exists", return_value=True)
-def test_get_marker_not_found(mock_spark):
+def test_get_marker_not_found(mock_table_exists, mock_spark):
     with patch("dbx_marker.manager.parse_marker_from_type", return_value=1) as _:
         manager = DbxMarker(delta_table_path="mock_path", spark=mock_spark)
         mock_spark.sql.return_value.first.return_value = None
         with pytest.raises(MarkerNotFoundError):
             manager.get_marker("non_existent_pipeline")
+
+
+@patch("dbx_marker.manager.delta_table_exists", return_value=True)
+def test_get_marker_raises_exception(mock_table_exists, mock_spark):
+    manager = DbxMarker(delta_table_path="mock_path", spark=mock_spark)
+    mock_spark.sql.side_effect = Exception("Test exception")
+    with pytest.raises(MarkerNotFoundError):
+        manager.get_marker("test_pipeline")
